a53acb3bc87583625ca7fe19248d19a2a6e0be66
Talet Pytago <90024069+talet1@users.noreply.github.com>
2023-12-18 11:38:49 +0700

    fix random function

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const quotes = [
     {
-      text: "Quote 1",
-      image: "materials/bacho1.jpg"
+        text: "Quote 1",
+        image: "materials/bacho1.jpg"
     },
     {
-      text: "Quote 2",
-      image: "materials/bacho2.jpg"
+        text: "Quote 2",
+        image: "materials/bacho2.jpg"
     },
     {
-      text: "Quote 3",
-      image: "materials/bacho3.jpg"
+        text: "Quote 3",
+        image: "materials/bacho3.jpg"
     },
     {
-      text: "Quote 4",
-      image: "materials/bacho4.jpg"
+        text: "Quote 4",
+        image: "materials/bacho4.jpg"
     },
     {
         text: "Quote 5",
@@ -39,13 +39,20 @@ const quotes = [
         text: "Quote 10",
         image: "materials/bacho10.jpg"
     }
-  ];
-  
+];
+
+let lastQuoteIndex = null;
+
 function generateRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * quotes.length);
+    } while (randomIndex === lastQuoteIndex);
+        
+    lastQuoteIndex = randomIndex;
     const quote = quotes[randomIndex];
     document.getElementById("quote-image").src = quote.image;
 }
-
+  
 generateRandomQuote();
 document.getElementById("new-quote-btn").addEventListener("click", generateRandomQuote);
